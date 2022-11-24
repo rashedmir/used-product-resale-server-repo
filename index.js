5cb48d54143c3796e04690d20c4061a3d9b36e2e
rashedmir <rashedmir89@gmail.com>
2022-11-25 00:42:11 +0600

    Database added

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 require('dotenv').config();
+
+
+const uri = "mongodb+srv://DB_USER:DB_PASSWORD@cluster0.klpdq3q.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const collection = client.db("usedProductResale").collection("watches");
+    }
+    finally{
+
+    }
+}
+run().catch(error => console.log(error))
+
 
 app.use(cors());
 app.use(express.json());
